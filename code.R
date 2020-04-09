ed947111671fd69e0676b4fcd7b4d882a9dbb5ca
Jake Wittman <wittm094@umn.edu>
2020-04-09 12:53:17 -0400

    Some %s are off so I'm going to adjust the pity timer to only replace commons

--- code.R
+++ code.R
@@ -27,26 +27,7 @@ pacman::p_load(tidyverse,
 # Make a data frame that has a row for every pack opened and a column for the number
 # of cards in each rarity. Use to make plotts and such
 
-# Probability calculations ------------------------------------------------
-# Looking at the data on the wiki, the percentage of total for the rarities is
-# pretty consistent, so I'll work with the one with most cards the GT
 
-total_cards <- 75545
-total_packs <- total_cards / 5
-common_cards <- 54271
-rare_cards <- 17276
-epic_cards <- 3232
-legendary_cards <- 766
-non_common_total <- sum(c(rare_cards, epic_cards, legendary_cards))
-
-common_perc <- common_cards / total_cards
-rare_perc <- rare_cards / total_cards
-epic_perc <- epic_cards / total_cards
-legendary_perc <- legendary_cards / total_cards
-
-marg_rare_perc <- rare_cards / non_common_total
-marg_epic_perc <- epic_cards / non_common_total
-marg_legendary_perc <- legendary_cards / non_common_total
 
 
 
@@ -54,6 +35,8 @@ marg_legendary_perc <- legendary_cards / non_common_total
 # Much faster pack opening function. The code is probably a bit harder to understand
 # and will take more effort to dig into, but should produce qualitatively similar
 # results as the much slower version.
+# Originally assumed the pity timer replaced a common or a rare card, but my %s were off 
+# so I'm going to try just replacing a common card when the pity timer activates.
 
 # Set the number of packs to open > 500 (probably 1000 for a normal set).
 openPack <- function(num.commons,
@@ -61,6 +44,27 @@ openPack <- function(num.commons,
                      num.epics,
                      num.legendaries,
                      num.packs) {
+  
+  # Probability calculations ------------------------------------------------
+  # Looking at the data on the wiki, the percentage of total for the rarities is
+  # pretty consistent. I'll just use the totals
+  
+  total_cards <- 163485
+  total_packs <- total_cards / 5
+  common_cards <- 117090
+  rare_cards <- 37355
+  epic_cards <- 7245
+  legendary_cards <- 1795
+  non_common_total <- sum(c(rare_cards, epic_cards, legendary_cards))
+  
+  common_perc <- common_cards / total_cards
+  rare_perc <- rare_cards / total_cards
+  epic_perc <- epic_cards / total_cards
+  legendary_perc <- legendary_cards / total_cards
+  
+  marg_rare_perc <- rare_cards / non_common_total
+  marg_epic_perc <- epic_cards / non_common_total
+  marg_legendary_perc <- legendary_cards / non_common_total
   
   # Data frame to hold data
   df <- as.data.frame(matrix(NA, ncol = 4, nrow = num.packs))
@@ -105,7 +109,7 @@ openPack <- function(num.commons,
   if (all(all_cards[4, 1:10] == 0)) {
     
     # Want to replaced one of the common or rare cards (assumed behavior)
-    num_rare_common <- all_cards[1, 10] + all_cards[2, 10]
+    num_rare_common <- all_cards[1, 10] 
     # Pick whether common or rare is replaced
     replace <- rmultinom(1, 1, prob = c(all_cards[1, 10] / num_rare_common,
                                         all_cards[2, 10] / num_rare_common,
@@ -166,7 +170,7 @@ openPack <- function(num.commons,
     # First instance of bad luck streak
     first_bad_luck <-  which(where_bad_luck > 9)[10]
     
-    # Want to replace one of the common or rare cards (assumed behavior)
+    # Want to replace one of the common cards (assumed behavior)
     num_rare_common <- all_cards[1, first_bad_luck] + all_cards[2, first_bad_luck]
     # Pick whether common or rare is replaced
     replace <- rmultinom(1, 1, prob = c(all_cards[1, first_bad_luck] / num_rare_common,
@@ -390,12 +394,12 @@ avgPacksForPerc <- function(list.out,
 
 #  Simulate ---------------------------------------------------------------
 
-# Number of cards in Descent of Dragons
-num_commons <- 98
-num_rares <- 72
-num_epics <- 54
-num_legendaries <- 28
-num_packs <- 5000
+# Number of cards in Ashes of Outland
+num_commons <- 104
+num_rares <- 70
+num_epics <- 46
+num_legendaries <- 25
+num_packs <- 1000
 
 set_nums <- c(num_commons,
               num_rares,
@@ -403,33 +407,33 @@ set_nums <- c(num_commons,
               num_legendaries,
               num_packs)
 
-num_sims <- 50
+num_sims <- 100
 sim_list <- rep(list(set_nums), num_sims)
 
 # Set up parallel simulations
 plan(multiprocess)
 
-microbenchmark(future_map(sim_list, function(.x) {
-  out <- openPack(
-    num.commons = .x[1],
-    num.rares = .x[2],
-    num.epics = .x[3],
-    num.legendaries = .x[4],
-    num.packs = .x[5]
-  )
-  return(out)
-},
-.progress = TRUE),
-map(sim_list, function(.x) {
-  out <- openPack(
-    num.commons = .x[1],
-    num.rares = .x[2],
-    num.epics = .x[3],
-    num.legendaries = .x[4],
-    num.packs = .x[5]
-  )
-  return(out)
-}), times = 10)
+# microbenchmark(future_map(sim_list, function(.x) {
+#   out <- openPack(
+#     num.commons = .x[1],
+#     num.rares = .x[2],
+#     num.epics = .x[3],
+#     num.legendaries = .x[4],
+#     num.packs = .x[5]
+#   )
+#   return(out)
+# },
+# .progress = TRUE),
+# map(sim_list, function(.x) {
+#   out <- openPack(
+#     num.commons = .x[1],
+#     num.rares = .x[2],
+#     num.epics = .x[3],
+#     num.legendaries = .x[4],
+#     num.packs = .x[5]
+#   )
+#   return(out)
+# }), times = 10)
 
 # Simulate pack opening until set is complete
 out <- future_map(sim_list, function(.x) {
@@ -455,11 +459,11 @@ set_perc_plot <- ggplot(plot_out, aes(x = num_packs, y = set_perc)) +
   geom_line(aes(group = sim), alpha = 0.25, colour = "grey") +
   geom_smooth(se = TRUE) +
   theme_bw() +
-  scale_x_continuous(breaks = seq(0, 250, 25),
-                     labels = seq(0, 250, 25),
-                     limits = c(0, 250)) +
-  geom_hline(yintercept = c(0.25, 0.5, 0.75, 1.0), linetype = "dashed") +
-  geom_vline(xintercept = c(25, 50, 75, 100), linetype = "dashed") +
+  scale_x_continuous(breaks = seq(0, 300, 25),
+                     labels = seq(0, 300, 25),
+                     limits = c(0, 300)) +
+  geom_hline(yintercept = c(0.25, 0.5, 0.75, 1.0), linetype = "dashed", alpha = 0.5) +
+  geom_vline(xintercept = c(25, 50, 75, 100), linetype = "dashed", alpha = 0.5) +
   labs(x = "Number of packs opened", y = "Percent of set obtained")
 
 
@@ -467,18 +471,19 @@ dust_plot <- ggplot(plot_out, aes(x = num_packs, y = total_dust)) +
   geom_line(aes(group = sim), alpha = 0.25, colour = "grey") +
   geom_smooth(se = TRUE) +
   theme_bw() +
-  scale_y_continuous(breaks = seq(0, 10000, 2500),
-                     labels = seq(0, 10000, 2500),
-                     limits = c(0, 10000)) +
-  scale_x_continuous(breaks = seq(0, 200, 25),
-                     labels = seq(0, 200, 25),
-                     limits = c(0, 200)) +
+  scale_y_continuous(breaks = seq(0, 15000, 2500),
+                     labels = seq(0, 15000, 2500),
+                     limits = c(0, 15000)) +
+  scale_x_continuous(breaks = seq(0, 300, 25),
+                     labels = seq(0, 300, 25),
+                     limits = c(0, 300)) +
   labs(x = "Number of packs opened", y = "Dust from disenchanting duplicates")
 
 
 p <- set_perc_plot + dust_plot
 
-p
+p + labs(caption = "\n Results based on a Hearthstone card ack opening simulator created by reddit user BagofAedeagi")
+
 
 
 
@@ -494,10 +499,37 @@ perc_summary
 
 avgPacksForPerc(out, 0.50)
 
+# See how percentages match up
+# Should see about 0.05 epics and 0.01 prob of legendary
 
-test %>% 
+# Probability a single card will be of a given rarity
+# Seems like I'm underestimating the amounts of commons and overestimating the others,
+# although most of the over estimation is from rares
+# From wiki it should be 71.6%, 22.85%, 4.43%, 1.10% for asecending rarity
+out %>% 
+  bind_rows(.id = "sim") %>% 
+  group_by(sim) %>% 
   summarise(common_prob = sum(commons) / sum(commons, rares, epics, legendaries),
             rare_prob = sum(rares) / sum(commons, rares, epics, legendaries),
             epic_prob = sum(epics) / sum(commons, rares, epics, legendaries),
-            legendary_prob = sum(legendaries) / sum(commons, rares, epics, legendaries))
+            legendary_prob = sum(legendaries) / sum(commons, rares, epics, legendaries)) %>% 
+  summarise_all(mean, na.rm = T) %>% 
+  select(-sim) %>% 
+  rbind(., c(0.716, 0.2285, 0.043, 0.011)) %>% 
+  as.data.frame(.) %>% 
+  round(., 4) %>% 
+  set_rownames(c("Obs", "Exp"))
 
+# Probability of getting at least one card of a given rarity in a single pack
+# Rare prob is low (should be ~ 95.66) while epic and legendary are too high
+# They should be 20.56 and 5.13 respectively
+out %>% 
+  bind_rows(.id = "sim") %>% 
+  summarise(common_prob = sum(commons >= 1) / n(),
+            rare_prob = sum(rares >= 1) / n(),
+            epic_prob = sum(epics >= 1) / n(),
+            legendary_prob = sum(legendaries >= 1) / n()) %>% 
+  rbind(., c(0.9997, 0.9566, 0.2056, 0.0513)) %>% 
+  as.data.frame(.) %>% 
+  round(., 4) %>% 
+  set_rownames(c("Obs", "Exp"))
